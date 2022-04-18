3c8bbd199dc18b4b7da5e878f583e837fc09209b
Furtifoppe <48923335+Furtifoppe@users.noreply.github.com>
2022-04-18 18:07:42 +0200

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,46 @@
+-- Les couleurs [info: J'utilise les Escape Character] --
+
+_G.colors = {
+
+    red = "\27[31m",
+    Blue = "\27[34m",
+    Green = "\27[32m"
+
+}
+
+--[[
+color = [vert, rouge, bleu] sur String
+timer = true ou false sur Boolean
+types = [info, log, error] sur String
+]]
+
+function Conf(color, types, text, timer) 
+
+
+    if timer == true then
+        timer = "["..os.date("%X").."]"
+    elseif timer == false then
+        timer = ""
+    end
+ 
+    if color == "rouge" then
+        color = _G.colors.red
+    elseif color == "vert" then
+        color = _G.colors.Green
+    elseif color == "bleu" then
+        color = _G.colors.Blue
+    end
+
+    if types == "info" then
+    types="[INFO]"
+    elseif types == "log" then
+    types="[LOG]"
+    elseif types == "error" then
+    types="[ERROR]"
+    end
+
+    print(color..timer..types..text)
+
+end
+
+
